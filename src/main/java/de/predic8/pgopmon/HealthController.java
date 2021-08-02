8e4eba3b17b2e42a9ce261fa864addf23c61529b
Valentin Brückel <brueckel@predic8.de>
2021-08-02 14:48:22 +0200

    Add /health endpoint for ReadinessProbe

---
+++ src/main/java/de/predic8/pgopmon/HealthController.java
@@ -0,0 +1,14 @@
+package de.predic8.pgopmon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+
+    @GetMapping(value = "/health", produces = "text/plain")
+    public String ready() {
+        return "OK\n";
+    }
+}
